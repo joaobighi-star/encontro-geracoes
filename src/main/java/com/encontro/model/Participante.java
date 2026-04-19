4f67502f83e776fc2d424cb7cb6f903d60c095a7
JoaoBighi <joaobighi@gmail.com>
2026-04-19 19:41:23 -0300

    Adequações

--- src/main/java/com/encontro/model/Participante.java
+++ src/main/java/com/encontro/model/Participante.java
@@ -13,7 +13,15 @@ public class Participante {
 
     private String nome;
 
-    private String idade;
+    private String faixaEtaria;
+
+    private String congrega;
+
+    private String vaiAlmocar;
+
+    private String atividade;
+
+    private String outraAtividade;
 
     private LocalDateTime dataInscricao;
 
@@ -22,37 +30,68 @@ public class Participante {
         this.dataInscricao = LocalDateTime.now();
     }
 
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getIdade() {
-		return idade;
-	}
-
-	public void setIdade(String idade) {
-		this.idade = idade;
-	}
-
-	public LocalDateTime getDataInscricao() {
-		return dataInscricao;
-	}
-
-	public void setDataInscricao(LocalDateTime dataInscricao) {
-		this.dataInscricao = dataInscricao;
-	}
-
     // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getFaixaEtaria() {
+        return faixaEtaria;
+    }
+
+    public void setFaixaEtaria(String faixaEtaria) {
+        this.faixaEtaria = faixaEtaria;
+    }
+
+    public String getCongrega() {
+        return congrega;
+    }
+
+    public void setCongrega(String congrega) {
+        this.congrega = congrega;
+    }
+
+    public String getVaiAlmocar() {
+        return vaiAlmocar;
+    }
+
+    public void setVaiAlmocar(String vaiAlmocar) {
+        this.vaiAlmocar = vaiAlmocar;
+    }
+
+    public String getAtividade() {
+        return atividade;
+    }
+
+    public void setAtividade(String atividade) {
+        this.atividade = atividade;
+    }
+
+    public String getOutraAtividade() {
+        return outraAtividade;
+    }
+
+    public void setOutraAtividade(String outraAtividade) {
+        this.outraAtividade = outraAtividade;
+    }
+
+    public LocalDateTime getDataInscricao() {
+        return dataInscricao;
+    }
+
+    public void setDataInscricao(LocalDateTime dataInscricao) {
+        this.dataInscricao = dataInscricao;
+    }
 }
